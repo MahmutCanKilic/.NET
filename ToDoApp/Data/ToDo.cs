29b6e54a6aeaa60922168f0d2ab90d773f342218
Murphyqq <kilicmahmut0@hotmail.com>
2023-06-13 15:16:17 +0300

    Data Access00

---
+++ ToDoApp/Data/ToDo.cs
@@ -0,0 +1,13 @@
+﻿namespace Data
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime DeletedTime { get; set;}
+        
+        
+        
+    }
+}
---
+++ ToDoApp/DataAccess/ToDoRepository.cs
@@ -0,0 +1,76 @@
+﻿using Data;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataAccess
+{
+    public class ToDoRepository
+    {
+        private List<ToDo> toDoList;
+        public ToDoRepository()
+        {
+            toDoList = new List<ToDo>();
+        }
+        private void AddToDo(int id, string description)
+        {
+            ToDo toDo = new ToDo();
+
+            toDo.Id = id;
+            toDo.Description = description;
+            DateTime createdTime = DateTime.Now;
+            toDo.CreatedTime = createdTime;
+
+            toDoList.Add(toDo);
+        }
+        private void DeleteToDo(int id, string description, DateTime deletedTime)
+        {
+            ToDo toDo = new ToDo();
+            toDo.Id = id;
+            toDo.Description = description;
+            deletedTime = DateTime.Now;
+            toDo.DeletedTime = deletedTime;
+            toDoList.Remove(toDo);
+        }
+        private ToDo FindWithId(int id)
+        {
+            foreach (var toDo in toDoList)
+            {
+                if (toDo.Id == id)
+                {
+                    return toDo;
+                }
+            }
+            return null;
+        }
+        private ToDo AllToDo(int id)
+        {
+            foreach (var toDo in toDoList)
+            {
+                return toDo;
+            }
+            return null;
+        }
+        private ToDo FindToDo(int id, string description, DateTime createdTime)
+        {
+            foreach (var toDo in toDoList)
+            {
+                if (toDo.Id == id || toDo.Description == description || toDo.CreatedTime == createdTime)
+                {
+                    return toDo;
+                }
+            }
+            return null;
+        }
+        private void UpdateToDo(int id, string description, DateTime createdTime)
+        {
+            foreach (var toDo in toDoList)
+            {
+                if (toDo.Id == id || toDo.CreatedTime == createdTime)
+                {
+                    toDoList.Remove(toDo);
+                    AddToDo(id, description);
+                }
+            }
+        }
+    }
+}
